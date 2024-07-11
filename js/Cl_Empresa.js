2323e5219b19385b4f8d58f4a75a61bbb1f959f5
ADCR20 <1001.31118017.ucla@gmail.com>
2024-07-11 10:49:01 -0400

    (Feature): Correccion de errores

--- js/Cl_Empresa.js
+++ js/Cl_Empresa.js
@@ -7,11 +7,12 @@ this.cont_empleados=0;
     procesar(e){
         this.cont_empleados++;
         if(e._sexo ==="M"){
-            this.cont_h++;
-            if(e._sexo ==="F")
+            this.cont_h++;}
+
+            if(e._sexo ==="F"){
             this.cont_m++;
         }
-    }
+        }
 devolverContH(){
     return this.cont_h;
 }
--- js/Principal.js
+++ js/Principal.js
@@ -4,13 +4,13 @@ de hombres que trabajan en la empresa y porcentaje de mujeres que trabajan en la
 Se dispone de los siguientes datos de varios empleados: (nombre, sexo) 
 (Mary, ‘F’), (José, ‘M’), (Carlos, ‘M’), (Pedro, ‘M’) */
 
-import Cl_Empleados from "./Cl_Empleados";
-import Cl_Empresa from "./Cl_Empresa";
+import Cl_Empleados from "./Cl_Empleados.js";
+import Cl_Empresa from "./Cl_Empresa.js";
 
-let empleado1=new Cl_Empleados (Mary, "F"); 
-let empleado2=new Cl_Empleados (José, "M"); 
-let empleado3=new Cl_Empleados (Carlos, "M");
-let empleado4=new Cl_Empleados (Pedro, "M");
+let empleado1=new Cl_Empleados ("Mary", "F"); 
+let empleado2=new Cl_Empleados ("José", "M"); 
+let empleado3=new Cl_Empleados ("Carlos", "M");
+let empleado4=new Cl_Empleados ("Pedro", "M");
 let empresa=new Cl_Empresa ();
 
 empresa.procesar(empleado1);
@@ -21,4 +21,4 @@ empresa.procesar(empleado4);
 let salida= document.getElementById("salida");
 salida.innerHTML= "Resultados";
 salida.innerHTML+= "<br> La cantidad de hombres que trabajan en la emprea es: " +empresa.devolverContH();
-salida.innerHTML+="<br> EL porcentaje de mujeres trabajando en la empresa es de: " +empresa.pcteMujeres();
+salida.innerHTML+="<br> EL porcentaje de mujeres trabajando en la empresa es de: " +empresa.pcteMujeres() +"%";
