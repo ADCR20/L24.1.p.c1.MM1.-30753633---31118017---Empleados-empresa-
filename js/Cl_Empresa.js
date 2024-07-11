f769285c4f7caa168dbf2b4a8b4d0a47a377fa3a
ADCR20 <1001.31118017.ucla@gmail.com>
2024-07-11 09:14:05 -0400

    (Feature)Inicio de ejercicio

---
+++ js/Cl_Empresa.js
@@ -0,0 +1,7 @@
+export default class Cl_Empresa{
+constructor(){
+this.cont_h=0;
+this.cont_m=0;
+this.cont_empleados=0;
+}
+}
---
+++ js/Principal.js
@@ -0,0 +1,6 @@
+/*Una empresa conoce de sus empleados los siguientes datos: nombre, sexo ((M)asculino
+(F)emenino). Se desea que realice un algoritmo que permite determinar e imprimir: Cantidad
+de hombres que trabajan en la empresa y porcentaje de mujeres que trabajan en la empresa.
+Se dispone de los siguientes datos de varios empleados: (nombre, sexo) 
+(Mary, ‘F’), (José, ‘M’), (Carlos, ‘M’), (Pedro, ‘M’) */
+
